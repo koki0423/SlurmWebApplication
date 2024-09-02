752556b1e10635003afc8cde2cae2ddba66a7cb6
Koki <al21034@shibaura-it.ac.jp>
2024-09-02 23:54:42 +0900

    first commit

--- src/test/java/jp/omochilab/slurmwebapplication/SlurmWebApplicationTests.java
+++ src/test/java/jp/omochilab/slurmwebapplication/SlurmWebApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SlurmWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
